509a50edc0e0fd385eb4b6c3ce4f5a2075a04cfc
Daudi Amani <dsaronin@gmail.com>
2011-02-22 13:33:10 -0800

    morphing base into stack model 8

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ProjectTemplate::Application.config.secret_token = '71e8e23f9ac2192b7d2830f1b96cc53494de8de7ca59d1fbf84c5c958e21146b94607cd9e99fb66418e3eb6b8fd9145328db40acab5b2479c17974f680fe7e27'
--- lib/mpango.rb
+++ lib/mpango.rb
@@ -1,19 +1,8 @@
 # ############################################################################
 # Mpango -- recurring/subscription billing gem for Rails Apps
 # => Version 1 focused on gateways like:
-# =>   PayPal Web Standard
+# =>   PayPal Web Standard		** default in version 1
 # =>   Amazon SimplePay
-# ############################################################################
-# ############################################################################
-# mpango stack (highest to lowest)  (SM = state machine)
-#
-#     tier                  SM? description
-# ----------------------------------------------------------------------------
-#  product feature          SM  product tiers for an account
-#  account product          SM  product billable transitions
-#  subscription billing     SM  generic recurring billing processing
-#  billing gateway              abstract interface to gateway 
-#  gateway access               specific gateway control
 # ############################################################################
 
 module Mpango
@@ -21,30 +10,3 @@ module Mpango
   
 end # module Mpango
 
-# ############################################################################
-# Stack Action Descriptions (from action trigger downwards thru stack)
-# 
-# when world account created, 
-#    Subscription.account_cart = Cart.new creates default cart for an account
-#    ProductFeature.new sets state
-#    AccountProduct.new sets state
-#    SubscriptionBilling.new sets state, 
-#     remembers asynch_purchasing_notification_url (controller level)
-#
-# when user clicks BUY NOW for product tier X
-#   capture info in upgrade_cart = Cart.new(params,
-#   product_feature.upgrade_subscription! 
-#      remember that payment process started
-#   account_product.upgrade_subscription!
-#   -- transitions :to => :billable_product, :from => [:unbillable_product, :trial_period, :disabled]
-#       subscription_billing.initiate_subscription!
-#       pay_button = gateway.initiate_subscription( cart )
-#         -- this is an encrypted POST FORM for the view;
-#            once user clicks the SUBSCRIBE button, user goes to PayPal site
-#            => if user cancels (doesnt do anything) ...?
-#              cron task needs to check for stranded requests
-#            => if user purchases, PayPal returns user to main site
-#              record the return
-#   -- transitions :to => :billable_product,   :from => :billable_product
-#       subscription_billing.change_subscription
-# 
