b7438612ac0ab1688d255b2e6bbb67c4f0ca7797
Daudi Amani <dsaronin@gmail.com>
2011-02-21 20:05:27 -0800

    morphing base into stack model 6

--- lib/mpango.rb
+++ lib/mpango.rb
@@ -1,5 +1,8 @@
 # ############################################################################
 # Mpango -- recurring/subscription billing gem for Rails Apps
+# => Version 1 focused on gateways like:
+# =>   PayPal Web Standard
+# =>   Amazon SimplePay
 # ############################################################################
 # ############################################################################
 # mpango stack (highest to lowest)  (SM = state machine)
@@ -22,32 +25,26 @@ end # module Mpango
 # Stack Action Descriptions (from action trigger downwards thru stack)
 # 
 # when world account created, 
-#    Subscription.account_cart = Cart.new creates default cart for lifetime of account
+#    Subscription.account_cart = Cart.new creates default cart for an account
 #    ProductFeature.new sets state
 #    AccountProduct.new sets state
 #    SubscriptionBilling.new sets state, 
-#     remembers asynch_purchasing_notification_url,
-    
-    
-
-# user clicks BUY NOW for product tier X
+#     remembers asynch_purchasing_notification_url (controller level)
+#
+# when user clicks BUY NOW for product tier X
 #   capture info in upgrade_cart = Cart.new(params,
-#   product_feature.upgrade_subscription! makes any changes product-wise up/down
+#   product_feature.upgrade_subscription! 
+#      remember that payment process started
 #   account_product.upgrade_subscription!
-#     if new
+#   -- transitions :to => :billable_product, :from => [:unbillable_product, :trial_period, :disabled]
 #       subscription_billing.initiate_subscription!
-#       return gateway.initiate_subscription( cart )
-#         -- they do everything ("button") gateways like PayPal/Amazon will return 
-#            an encrypted POST FORM for the view;
+#       pay_button = gateway.initiate_subscription( cart )
+#         -- this is an encrypted POST FORM for the view;
 #            once user clicks the SUBSCRIBE button, user goes to PayPal site
 #            => if user cancels (doesnt do anything) ...?
 #              cron task needs to check for stranded requests
-#            => if user purchases, PayPal returns user to Majozi site
+#            => if user purchases, PayPal returns user to main site
 #              record the return
-#         -- we do everything ("self_form") gateways will just return the cart
-#            the controller will have to somehow know what to do
-#            when user agrees to purchase, return to this part of processing stack
-#     else
+#   -- transitions :to => :billable_product,   :from => :billable_product
 #       subscription_billing.change_subscription
-#     end   # 
-  # 
+# 
--- lib/mpango/freemium/credit_card.rb
+++
@@ -1,4 +0,0 @@
-module Mpango
-  # I've vendor'd ActiveMerchant's CreditCard functionality, but I'd still like to alias it into the Mpango namespace.
-  class CreditCard < ::ActiveMerchant::Billing::CreditCard; end
-end
